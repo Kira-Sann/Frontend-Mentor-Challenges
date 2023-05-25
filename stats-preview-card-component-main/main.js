29762bc84c5102a35dfb2ca19c4bfc85dc610319
welliton santos <114530718+Kira-Sann@users.noreply.github.com>
2023-05-24 21:43:35 -0300

    update

--- stats-preview-card-component-main/main.js
+++ stats-preview-card-component-main/main.js
@@ -1,10 +1,8 @@
 const html = document.querySelector('html')
 const btn = document.querySelector('.btn')
 
-// Define uma função para alternar o modo escuro
 function toggleDarkMode() {
     html.classList.toggle('dark-mode')
   }
 
-// Adiciona um ouvinte de eventos ao botão para chamar a função toggleDarkMode quando o botão for alterado
   btn.addEventListener('click', toggleDarkMode)
